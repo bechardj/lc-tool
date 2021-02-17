1114f0d922dde21bacbcbc5206ad61a2fdd77823
Joey Bechard <jjb@uconn.edu>
2021-02-17 11:51:58 -0500

    add charts for more specific category breakdowns

--- src/main/resources/static/js/statistics.js
+++ src/main/resources/static/js/statistics.js
@@ -1,10 +1,10 @@
 let statistics;
 
-const canvas = document.getElementById("characterChart");
-const ctx = canvas.getContext("2d");
+
 
 function graph() {
-    let chart = new Chart(ctx, {
+    const allCharCtx = $("#allCharacterChart")[0].getContext("2d");
+    let chart = new Chart(allCharCtx, {
         type: 'bar',
         data: {
             labels: Object.keys(statistics.labelFrequency),
@@ -16,6 +16,66 @@ function graph() {
             }]
         },
         options: {
+            legend: {
+                display: false,
+            }
+        }
+    });
+
+    const upperCharCtx = $("#upperCharacterChart")[0].getContext("2d");
+    let upperChart = new Chart(upperCharCtx, {
+        type: 'bar',
+        data: {
+            labels: Object.keys(statistics.upperFrequency),
+            datasets: [{
+                label: 'Character Label Frequency',
+                backgroundColor: 'rgb(217,52,90)',
+                borderColor: 'rgb(217,52,90)',
+                data: Object.values(statistics.upperFrequency)
+            }]
+        },
+        options: {
+            legend: {
+                display: false,
+            }
+        }
+    });
+
+    const lowerCharCtx = $("#lowerCharacterChart")[0].getContext("2d");
+    let lowerChart = new Chart(lowerCharCtx, {
+        type: 'bar',
+        data: {
+            labels: Object.keys(statistics.lowerFrequency),
+            datasets: [{
+                label: 'Character Label Frequency',
+                backgroundColor: 'rgb(217,52,90)',
+                borderColor: 'rgb(217,52,90)',
+                data: Object.values(statistics.lowerFrequency)
+            }]
+        },
+        options: {
+            legend: {
+                display: false,
+            }
+        }
+    });
+
+    const punctuationCharCtx = $("#punctuationCharCtx")[0].getContext("2d");
+    let punctuationChart = new Chart(punctuationCharCtx, {
+        type: 'bar',
+        data: {
+            labels: Object.keys(statistics.punctuationFrequency),
+            datasets: [{
+                label: 'Character Label Frequency',
+                backgroundColor: 'rgb(217,52,90)',
+                borderColor: 'rgb(217,52,90)',
+                data: Object.values(statistics.punctuationFrequency)
+            }]
+        },
+        options: {
+            legend: {
+                display: false,
+            },
         }
     });
 }
