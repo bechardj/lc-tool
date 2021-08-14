edb95bb505ae6877f8fd97cb7b1144188ae8d869
Joey Bechard <jjb@uconn.edu>
2021-08-14 00:25:40 -0400

    remove vestigial logic from prediction engine used for loading models in the desktop/local version of the tool

--- src/main/resources/static/js/capture/predictionEngine.js
+++ src/main/resources/static/js/capture/predictionEngine.js
@@ -1,4 +1,5 @@
-class PredictionEngine{
+class PredictionEngine {
+
     constructor() {
         this.model = undefined;
         this.labels = undefined;
@@ -7,34 +8,14 @@ class PredictionEngine{
     async init() {
         try {
             await tf.ready();
-            await tf.loadLayersModel('/localModels/model.json')
+            await tf.loadLayersModel('/ml/model.json')
                 .then(model => {
-                    console.log("Loaded local model.");
                     this.model = model;
-                    this.retrieveLocalLabels();
-                })
-                .catch(err => {
-                    console.log("Falling back to included model...");
-                    tf.loadLayersModel('/ml/model.json')
-                        .then(model => {
-                            this.model = model;
-                            this.retrieveIncludedLabels();
-                        });
+                    this.retrieveIncludedLabels();
                 });
         } catch (e) {
             console.error("Failed to initialize prediction engine!");
             return false;
-        }
-        console.log(this.labels);
-        return true;
-    }
-
-    validateLabels(labels) {
-        for (let i = 0; i < labels.length; i++) {
-            if(labels[i].length !== 1) {
-                console.error("Encountered label longer than length 1 at index ", i);
-                return false;
-            }
         }
         return true;
     }
@@ -51,22 +32,6 @@ class PredictionEngine{
             });
     }
 
-    retrieveLocalLabels() {
-        $.getJSON("/localModels/labels.json",
-            (response) => {
-                if (!this.validateLabels(response.predictionLabels)) {
-                    console.log("Falling back to included labels (prediction result max index corresponds to 26 lower characters a-z)")
-                    this.retrieveIncludedLabels();
-                }
-            })
-            .fail(( jqxhr, textStatus, error ) => {
-                let err = textStatus + ", " + error;
-                console.warn("WARNING: You are using a local model without providing a labels.json file \n"
-                    + "To see an example, look at http://" + window.location.host + "/ml/labels.json");
-                console.log("Falling back to included labels (prediction result max index corresponds to 26 lower characters a-z)")
-                this.retrieveIncludedLabels();
-            });
-    }
 
     async tensorFlowPrediction(img) {
         let input = tf.browser.fromPixels(img).mean(2)
@@ -79,9 +44,8 @@ class PredictionEngine{
         let predictionData = await prediction.data();
         let index_label = predictionData.indexOf(Math.max(...predictionData));
         return this.labels[index_label];
-
     }
 
 }
 
-export {PredictionEngine};
+export { PredictionEngine };
