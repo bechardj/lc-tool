2d7112daf6d44b5726e9a39cf531d242d255c72f
Joey Bechard <jjb@uconn.edu>
2021-08-30 02:15:12 -0400

    missed abstract class in previous commit

--- src/main/java/us/jbec/lct/models/capture/CaptureData.java
+++ src/main/java/us/jbec/lct/models/capture/CaptureData.java
@@ -1,7 +1,14 @@
 package us.jbec.lct.models.capture;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public abstract class CaptureData implements Cloneable {
+
+    @NotBlank
     private String uuid;
+
+    @NotNull
     private CaptureDataRecordType captureDataRecordType;
 
     public CaptureData() {
@@ -26,5 +33,12 @@ public abstract class CaptureData implements Cloneable {
 
     public void setCaptureDataRecordType(CaptureDataRecordType captureDataRecordType) {
         this.captureDataRecordType = captureDataRecordType;
+    }
+
+    public CaptureData clone() throws CloneNotSupportedException {
+        CaptureData clone = (CaptureData) super.clone();
+        clone.setUuid(this.getUuid());
+        clone.setCaptureDataRecordType(this.getCaptureDataRecordType());
+        return clone;
     }
 }
