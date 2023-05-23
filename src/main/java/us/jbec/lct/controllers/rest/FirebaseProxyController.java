03b30a1db795121f18ac48d95eda6376bef826f3
Joey Bechard <jjb@uconn.edu>
2023-05-22 23:25:00 -0400

    post for proxy, add query params

--- src/main/java/us/jbec/lct/controllers/rest/FirebaseProxyController.java
+++ src/main/java/us/jbec/lct/controllers/rest/FirebaseProxyController.java
@@ -1,11 +1,12 @@
 package us.jbec.lct.controllers.rest;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @RestController
 public class FirebaseProxyController {
@@ -15,8 +16,22 @@ public class FirebaseProxyController {
     @GetMapping("/__/auth/**")
     public ResponseEntity<String> firebaseProxy(HttpServletRequest request) {
         String path = request.getRequestURI();
-        ResponseEntity<String> response = restTemplate.getForEntity("https://lc-tool.firebaseapp.com" + path, String.class);
+        String query = request.getQueryString();
+        query = query != null ? ("?" + query) : "";
+        ResponseEntity<String> response = restTemplate.getForEntity("https://lc-tool.firebaseapp.com" + path + query, String.class);
         return response;
+    }
+
+    @PostMapping("/__/auth/**")
+    public String firebaseProxyPost(HttpServletRequest request,
+                                                    @RequestBody(required = false) String body,
+                                                    @RequestHeader MultiValueMap<String, String> headers) {
+        String path = request.getRequestURI();
+        String query = request.getQueryString();
+        query = query != null ? ("?" + query) : "";
+        HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+
+        return restTemplate.postForObject("https://lc-tool.firebaseapp.com" + path + query, httpEntity, String.class);
     }
 
 }
